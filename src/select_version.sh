8517195431b73f8f6b928a0dc59d8d52722c733f
Stefan Muenzel <source@s.muenzel.net>
2025-06-15 09:24:21 +0200

    Version check

--- src/select_version.sh
+++ src/select_version.sh
@@ -3,7 +3,8 @@
 CUTOFF_VERSION=$1
 PRE_VERSION_FILE=$2
 POST_VERSION_FILE=$3
-ACTUAL_VERSION=$4
+## replace '~' by space
+ACTUAL_VERSION=$(echo $4 | sed 's/~/ /g')
 
 if [ "$(printf '%s\n' "$ACTUAL_VERSION" "$CUTOFF_VERSION" | sort -V | head -n1)" = "$CUTOFF_VERSION" ]; then
   echo "(* Selected $POST_VERSION_FILE because $ACTUAL_VERSION >= $CUTOFF_VERSION *)"
