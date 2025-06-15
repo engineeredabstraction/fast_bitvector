7a93b3af3a0d1a6b4a203baec02686df0bb46384
Stefan Muenzel <source@s.muenzel.net>
2025-06-15 12:29:14 +0200

    Fix version select

--- src/select_version.sh
+++ src/select_version.sh
@@ -6,7 +6,10 @@ POST_VERSION_FILE=$3
 ## replace '~' by space
 ACTUAL_VERSION=$(echo $4 | sed 's/~/ /g')
 
-if [ "$(printf '%s\n' "$ACTUAL_VERSION" "$CUTOFF_VERSION" | sort -V | head -n1)" = "$CUTOFF_VERSION" ]; then
+if [ -z "$ACTUAL_VERSION" ]; then
+  echo "(* Selected $POST_VERSION_FILE because version is not set *)"
+  cat $POST_VERSION_FILE
+elif [ "$(printf '%s\n' "$ACTUAL_VERSION" "$CUTOFF_VERSION" | sort -V | head -n1)" = "$CUTOFF_VERSION" ]; then
   echo "(* Selected $POST_VERSION_FILE because $ACTUAL_VERSION >= $CUTOFF_VERSION *)"
   cat $POST_VERSION_FILE
 else
