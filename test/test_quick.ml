94ab3c7af74e39316a616a1fd966403f55de08fb
Stefan Muenzel <source@s.muenzel.net>
2025-06-17 11:22:52 +0200

    More equality

--- test/test_quick.ml
+++ test/test_quick.ml
@@ -1,12 +1,13 @@
-open Sexplib
-open Sexplib0.Sexp_conv
+open Core
 
 let test_op1 ~op_bv ~op_bool bl =
   let bv = Fast_bitvector.Bit_zero_first.of_bool_list bl in
   let bl' = ListLabels.map bl ~f:op_bool in
   let bv' = op_bv bv in
   let bl'_as_bv = Fast_bitvector.Bit_zero_first.of_bool_list bl' in
-  Fast_bitvector.equal bv' bl'_as_bv
+  let eq_bv = Fast_bitvector.equal bv' bl'_as_bv in
+  let eq_list = [%equal: bool list] bl' (Fast_bitvector.Bit_zero_first.to_bool_list bv') in
+  eq_bv && eq_list
 
 let test_op2 ~op_bv ~op_bool (bl1, bl2) =
   let bv1 = Fast_bitvector.Bit_zero_first.of_bool_list bl1 in
@@ -14,12 +15,14 @@ let test_op2 ~op_bv ~op_bool (bl1, bl2) =
   let bl' = ListLabels.map2 bl1 bl2 ~f:op_bool in
   let bv' = op_bv bv1 bv2 in
   let bl'_as_bv = Fast_bitvector.Bit_zero_first.of_bool_list bl' in
-  Fast_bitvector.equal bv' bl'_as_bv
+  let eq_bv = Fast_bitvector.equal bv' bl'_as_bv in
+  let eq_list = [%equal: bool list] bl' (Fast_bitvector.Bit_zero_first.to_bool_list bv') in
+  eq_bv && eq_list
 
 let%test_unit "not" =
   QCheck.Test.make ~count:1000 ~name:"not" 
     QCheck.(list bool)
-    (test_op1 ~op_bv:Fast_bitvector.Allocate.not ~op_bool:Bool.not)
+    (test_op1 ~op_bv:Fast_bitvector.Allocate.not ~op_bool:Stdlib.Bool.not)
   |> QCheck.Test.check_exn
 
 let test_binary ~name ~op_bv ~op_bool =
@@ -47,5 +50,5 @@ let%test_unit "or" =
   test_binary ~name:"or" ~op_bv:Fast_bitvector.Allocate.or_ ~op_bool:(||)
 
 let%test_unit "xor" =
-  test_binary ~name:"xor" ~op_bv:Fast_bitvector.Allocate.xor ~op_bool:(fun a b -> a <> b)
+  test_binary ~name:"xor" ~op_bv:Fast_bitvector.Allocate.xor ~op_bool:Bool.(<>)
 
