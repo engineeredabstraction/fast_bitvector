d351dc090444f4307be1218e1939ac4887adaefc
Stefan Muenzel <source@s.muenzel.net>
2025-06-20 15:47:09 +0200

    clz

--- src/bitops.caml.ml
+++ src/bitops.caml.ml
@@ -42,3 +42,40 @@ let count_trailing_zeros_64 (x : int64) : int =
   let product = Int64.mul isolated deBruijn in
   let index = Int64.(to_int (shift_right_logical product 58)) in
   Array.unsafe_get ctz64_table index
+
+let clz32_table = [|
+  31; 22; 30; 21; 18; 10; 29;  2; 20; 17; 15; 13;  9;  6; 28;  1;
+  23; 19; 11;  3; 12; 16; 14;  7; 24;  4;  8; 25;  5; 26; 27;  0
+|]
+
+let count_leading_zeros_32 (x : int32) : int =
+  let x = Int32.logor x (Int32.shift_right_logical x 1) in
+  let x = Int32.logor x (Int32.shift_right_logical x 2) in
+  let x = Int32.logor x (Int32.shift_right_logical x 4) in
+  let x = Int32.logor x (Int32.shift_right_logical x 8) in
+  let x = Int32.logor x (Int32.shift_right_logical x 16) in
+  let y = Int32.logand x (Int32.lognot (Int32.shift_right_logical x 1)) in
+  let deBruijn = 0x07C4ACDDl in
+  let product = Int32.mul y deBruijn in
+  let index = Int32.to_int (Int32.shift_right_logical product 27) in
+  Array.unsafe_get clz32_table index
+
+let clz64_table = [|
+  63;  5; 62;  4; 16; 10; 61;  3; 24; 15; 36;  9; 30; 21; 60;  2;
+  12; 26; 23; 14; 45; 35; 43;  8; 33; 29; 52; 20; 58; 59;  1; 51;
+  6; 13; 25; 44; 34; 32; 28; 57; 50;  7; 11; 22; 46; 42; 31; 27;
+  53; 19; 56; 49; 54; 18; 48; 17; 47; 41; 55; 40; 39; 38; 37;  0
+|]
+
+let count_leading_zeros_64 (x : int64) : int =
+  let x = Int64.logor x (Int64.shift_right_logical x 1) in
+  let x = Int64.logor x (Int64.shift_right_logical x 2) in
+  let x = Int64.logor x (Int64.shift_right_logical x 4) in
+  let x = Int64.logor x (Int64.shift_right_logical x 8) in
+  let x = Int64.logor x (Int64.shift_right_logical x 16) in
+  let x = Int64.logor x (Int64.shift_right_logical x 32) in
+  let y = Int64.logand x (Int64.lognot (Int64.shift_right_logical x 1)) in
+  let deBruijn = 0x03F79D71B4CB0A89L in
+  let product = Int64.mul y deBruijn in
+  let index = Int64.to_int (Int64.shift_right_logical product 58) in
+  Array.unsafe_get clz64_table index
